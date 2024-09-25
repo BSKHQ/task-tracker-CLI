7182581a84894cacc0513c915a96388f62c2f4aa
BSKHQ <kunlebakaresodiq@gmail.com>
2024-09-25 13:46:17 +0100

    the beginning

---
+++ src/main/java/com/bskhq/tasktrackercli/TaskTrackerCli.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bskhq.tasktrackercli;
+
+/**
+ *
+ * @author kunle
+ */
+public class TaskTrackerCli {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
