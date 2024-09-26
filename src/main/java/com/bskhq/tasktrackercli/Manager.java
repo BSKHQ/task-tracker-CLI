53c95b80064e61c85007959181c77530ec7601d3
BSKHQ <kunlebakaresodiq@gmail.com>
2024-09-26 14:42:37 +0100

    added necessary methods to Manager class

--- src/main/java/com/bskhq/tasktrackercli/Manager.java
+++ src/main/java/com/bskhq/tasktrackercli/Manager.java
@@ -10,5 +10,44 @@ package com.bskhq.tasktrackercli;
  * @author kunle
  */
 public class Manager {
+    /*manages Tasks */
+    public Manager(){
 
+    }
+
+    public void addTask(Task task){
+
+    }
+
+    public void updateTask(int id, String newDescription){
+
+    }
+
+    public void deleteTask(int id){
+
+    }
+
+    public void listAllTasks(){
+
+    }
+
+    public void listDoneTasks(){
+
+    }
+
+    public void listToDoTasks(){
+
+    }
+
+    public void listInProgressTasks(){
+
+    }
+
+    public void markTaskAsInProgress(){
+
+    }
+
+    public void markTaskAsDone(){
+
+    }
 }
